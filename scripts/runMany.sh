0196e9fff093f13f23ca3e04f19a8fa9fdc23425
Aaron Elmore <aelmore@csail.mit.edu>
2014-06-09 14:49:37 -0400

    long run

--- scripts/runMany.sh
+++ scripts/runMany.sh
@@ -6,10 +6,10 @@ python scripts/multi.py -c -n 0
 rm *log
 
 CRT="python scripts/multi.py -n "
-RUN="java -cp target/manydbbench-1.0-SNAPSHOT-jar-with-dependencies.jar edu.mit.csail.ManyBench -t 30 -n "
+RUN="java -cp target/manydbbench-1.0-SNAPSHOT-jar-with-dependencies.jar edu.mit.csail.ManyBench -t 120 -n "
 
 CNT=0
-DBSTOADD=( 1 2 1)
+DBSTOADD=( 1 9 90 100 300 500 1500 2500 2500 2500)
 
 
 
